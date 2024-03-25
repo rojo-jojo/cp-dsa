77dc698a37cccaf086c923dd22242be4879cc84c
zima-small <40956690+rojo-jojo@users.noreply.github.com>
2024-03-25 14:52:35 +0530

    commit

---
+++ unique_paths.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> vec(m, vector<int> (n, 0));
+        // cout << m << ", " << n;
+        for (int i=m-1; i>=0; i--) {
+            for (int j=n-1; j >= 0; j--) {
+                // cout << i << "," << j;
+                if (i == m-1 && j == n-1) vec[i][j] = 1;
+                else if (i == m-1) vec[m-1][j] = 1;
+                else if (j == n-1) vec[i][n-1] = 1;
+                else {
+                    vec[i][j] = vec[i][j+1] + vec[i+1][j];
+                }
+            }
+        }
+        return vec[0][0];
+    }
+};
